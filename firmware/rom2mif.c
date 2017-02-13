fa984270f76466f10869752987543733aa543fac
Zhiyuan Wan <rgwan@rocaloid.org>
2017-02-13 18:19:08 +0800

    rom2mif: Code beautify

--- firmware/rom2mif.c
+++ firmware/rom2mif.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    FILE *fp, *fp1, *fp2, *fp3, *fp4;
-    fp = fopen("firmware.bin", "rb");
-    fp1 = fopen("hi.mif", "w+");
-    fp2 = fopen("mh.mif", "w+");
-    fp3 = fopen("ml.mif", "w+");
-    fp4 = fopen("lo.mif", "w+");
-    if(!fp)
+	FILE *fp, *fp1, *fp2, *fp3, *fp4;
+	if(argc != 6)
+	{
+		printf("Usage %s: <input-binary> <24~32bit MIF file> <16~24 bit MIF file> <8~16 bit MIF file> <1~8 bit MIF file>\n", argv[0]);
+		printf("Copyright Zhiyuan Wan 2017\n");
+		return -1;
+	}
+	fp =  fopen(argv[1], "rb");
+	fp1 = fopen(argv[2], "w+");
+	fp2 = fopen(argv[3], "w+");
+	fp3 = fopen(argv[4], "w+");
+	fp4 = fopen(argv[5], "w+");
+	if(!fp)
 	{
 		printf("File doesn't exist!\n");
 		return -1;
@@ -18,7 +24,7 @@ int main()
 	int size = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
 	printf("Binary length: %d bytes\n", size);
-
+	printf("MIF files %s %s %s %s\r\n", argv[2], argv[3], argv[4], argv[5]);
 	uint8_t dat[4];
 	int i = 0;
 	fprintf(fp1, "DEPTH = 1024;\nWIDTH = 8;\n"
